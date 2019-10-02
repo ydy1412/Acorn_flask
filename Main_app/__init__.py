01b29253e42327a55ff8275af4f36a60add1aa56
ydy1412 <ydy1412@naver.com>
2019-10-02 15:51:52 +0900

    change start_Main_app.py

--- Main_app/__init__.py
+++ Main_app/__init__.py
@@ -1,8 +1,19 @@
-from flask import Flask
+from flask import Flask,g
 
 app =Flask(__name__)
+app.debug = True
+
+@app.before_first_request
+def before_request():
+    print("before_request!!")
+    # 서버를 제어하고 싶을때 주로 사용됨.
+    g.str = '한글'
+
+@app.route("/gg")
+def helloworld():
+    return "hello Flask World!" + getattr(g,'str','111')
 
 @app.route("/")
-def helloworld():
+def helloworld2():
     return "hello Flask World!"
 
--- start_Main_app.py
+++ start_Main_app.py
@@ -1,3 +1,3 @@
 from Main_app import app
 
-app.run(host='0,0,0,0')
+app.run(host='0.0.0.0')
