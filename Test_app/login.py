9d186bfd3fa2563ba6aca9a6214eb7ae8b826900
ydy1412 <ydy1412@naver.com>
2019-11-08 01:00:03 +0900

    sdf

--- Test_app/login.py
+++ Test_app/login.py
@@ -3,16 +3,18 @@ from pymongo import MongoClient
 
 app =Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8zdkjsfhqw]/'
-
-client = MongoClient('mongodb://localhost:27017')
-if "member" not in client.list_database_names() :
-    db = client['member']
-    collect = db['Account']
-    data = {'id': 'ydy1412', 'password': '920910'}
-    collect.insert_one(data)
-    client.close()
-else :
-    client.close()
+try :
+    client = MongoClient('mongodb://localhost:27017')
+    if "member" not in client.list_database_names():
+        db = client['member']
+        collect = db['Account']
+        data = {'id': 'ydy1412', 'password': '920910'}
+        collect.insert_one(data)
+        client.close()
+    else:
+        client.close()
+except :
+    print("db problem happend")
 
 @app.route("/")
 def helloworld2():
@@ -49,6 +51,46 @@ def member_checked():
     else :
         return "no id"
 
+@app.route('/sur', methods=['GET', 'POST'])
+def test1():
+    if request.method == 'POST':
+
+        ProblemSolving = request.form["ProblemSolving"]
+        BuildingThings = request.form["BuildingThings"]
+        LearningNewTech =request.form["LearningNewTech"]
+        BoringDetails = request.form["BoringDetails"]
+        JobSecurity = request.form["JobSecurity"]
+        DiversityImportant=request.form["DiversityImportant"]
+        AnnoyingUI=request.form["AnnoyingUI"]
+        FriendsDevelopers=request.form["FriendsDevelopers"]
+        RightWrongWay=request.form["RightWrongWay"]
+        UnderstandComputers=request.form["UnderstandComputers"]
+        SeriousWork=request.form["SeriousWork"]
+        InvestTimeTools=request.form["InvestTimeTools"]
+        WorkPayCare=request.form["WorkPayCare"]
+        ChallengeMyself=request.form["ChallengeMyself"]
+        CompetePeers=request.form["CompetePeers"]
+        ChangeWorld=request.form["ChangeWorld"]
+        AssessJobRole=request.form["AssessJobRole"]
+        AssessJobRemote=request.form["AssessJobRemote"]
+        AssessJobProduct=request.form["AssessJobProduct"]
+        AssessJobProfDevel=request.form["AssessJobProfDevel"]
+        ImportantHiringEducation=request.form["ImportantHiringEducation"]
+        ImportantHiringCommunication=request.form["ImportantHiringCommunication"]
+        EducationTypes=request.form["EducationTypes"]
+        ImportantBenefits=request.form["ImportantBenefits"]
+        FormalEducation=request.form["FormalEducation"]
+        MajorUndergrad=request.form["MajorUndergrad"]
+        result = {"ProblemSolving": ProblemSolving, "BuildingThings": BuildingThings,"LearningNewTech":LearningNewTech,"BoringDetails":BoringDetails,
+                  "JobSecurity":JobSecurity,"DiversityImportant":DiversityImportant,"AnnoyingUI": AnnoyingUI ,"FriendsDevelopers":FriendsDevelopers,
+                  "RightWrongWay":RightWrongWay,"UnderstandComputers":UnderstandComputers,"SeriousWork":SeriousWork,"InvestTimeTools":InvestTimeTools,
+                  "WorkPayCare":WorkPayCare,"ChallengeMyself":ChallengeMyself,"CompetePeers":CompetePeers,"ChangeWorld":ChangeWorld,
+                  "AssessJobRole":AssessJobRole,"AssessJobRemote":AssessJobRemote,"AssessJobProduct":AssessJobProduct,"AssessJobProfDevel":
+                  AssessJobProfDevel,"ImportantHiringEducation":ImportantHiringEducation,"ImportantHiringCommunication":ImportantHiringCommunication,
+                  "EducationTypes":EducationTypes,"ImportantBenefits":ImportantBenefits,"FormalEducation":FormalEducation,"MajorUndergrad":MajorUndergrad}
+        return result
+
+    return render_template("Q1.html")
 
 if __name__ == "__main__" :
     app.debug = True
--- Test_app/mongodb_control.py
+++ Test_app/mongodb_control.py
@@ -1,14 +1,19 @@
 from pymongo import MongoClient
-client = MongoClient("mongodb://localhost:27017/")
-databases = client.list_database_names()
-db = client.get_database("member")
-collections = db.get_collection('Account')
-login_data = {"id": 'ydy1412',"password" : '920910'}
-Id_existence = collections.find_one(login_data)
-print(Id_existence)
-# data = {"author" : "Mike",'text' : "my first blog post!"}
-# collect.insert_one(data)
-# Str = ""
-# for i in collect.find() :
-#     Str += str(i)
-client.close()
+import sys
+def create_account(id,password) :
+    client = MongoClient("mongodb://localhost:27017/")
+    databases = client.list_database_names()
+    db = client.get_database("member")
+    collections = db.get_collection('Account')
+    login_data = {"id": id, "password": password}
+    Id_existence = collections.find_one(login_data)
+    if Id_existence == True:
+        print("Id is already existed")
+    else :
+        collections.insert_one(login_data)
+        print("Id created")
+    client.close()
+
+if __name__ == "__main__" :
+    create_account(sys.argv[1],sys.argv[2])
+
